cd8b57c81834ad7932a2254f5fe45735a0d40a2d
Łukasz Majewski <lukasz.majewski@sumito.uk>
2022-03-16 16:46:58 +0100

    Change dependency for our fork

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/ajamaica/Solana.Swift", branch: "master")
+         .package(url: "https://github.com/ldmajewski/Solana.Swift", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
